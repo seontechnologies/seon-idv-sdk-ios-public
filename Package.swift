29cb6125acb4e44efea67f6c35b316fd76ae04d8
Abbas <abbas.sabetinezhad@seon.io>
2025-07-24 08:10:28 +0200

    Release: v2.0.1

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SEONIdVerification",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v16)
     ],
     products: [
         .library(
@@ -15,24 +15,29 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/BlinkID/blinkid-ios", from: "6.11.1"),
         .package(url: "https://github.com/BlinkID/capture-core-sp", from: "1.3.1"),
         .package(url: "https://github.com/BlinkID/capture-ux-sp", from: "1.3.1"),
+        .package(url: "https://github.com/hmlongco/Resolver", from: "1.5.1"),
     ],
     targets: [
         .target(
             name: "SEONIDV",
             dependencies: [
                 .target(name: "SEONIdVerification"),
-                .product(name: "BlinkID", package: "blinkid-ios"),
+                .target(name: "BlinkID"),
                 .product(name: "CaptureCore", package: "capture-core-sp"),
                 .product(name: "CaptureUX", package: "capture-ux-sp"),
             ]
         ),
         .binaryTarget(
             name: "SEONIdVerification",
-            url: "https://s3.us-east-1.amazonaws.com/idv-static.native-sdk.seondev.space/sdk-releases/1.6.1/SEONIdVerification_SPM.zip",
-            checksum: "e4405528e014869b60663a7165f7623edb0262006c0b29780cb8bdf90b37af8e"
+            url: "https://github.com/seontechnologies/seon-idv-sdk-ios-public/releases/download/v2.0.1/IDV_SPM_Modern.zip",
+            checksum: "862302390aaa60cc205032e958f593a423c6faf78246579647777456e73fc0a2"
+        ),
+        .binaryTarget(
+            name: "BlinkID",
+            url: "https://github.com/BlinkID/blinkid-ios/releases/download/v7.3.0/BlinkID.xcframework.zip",
+            checksum: "61b35749e99a2df0d609359170b76d5c72279398391b6e907aaf073eaf972476"
         )
     ]
 )
